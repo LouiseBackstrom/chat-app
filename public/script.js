c1c4f2222686aaa0290f9c6d56a49b67431b1a0e
Louise Bäckström <backstromlouise@gmail.com>
2020-04-27 17:22:26 +0200

    cleaned code

--- public/script.js
+++ public/script.js
@@ -5,31 +5,33 @@ window.addEventListener('load', () => {
 })
 
 function setupEventListeners() {
-  // Join submit handler
-  const joinForm = document.querySelector('form.join.ui')
-  joinForm.addEventListener('submit', onJoinRoom)
+  // Join on submit
+  const joinChat = document.querySelector('form.join')
+  joinChat.addEventListener('submit', onJoinRoom)
 
-  // Send message submit handler
-  const messageForm = document.querySelector('.chat.ui form')
-  messageForm.addEventListener('submit', onSendMessage)
+  // Send message on submit
+  const messageChat = document.querySelector('.chat form')
+  messageChat.addEventListener('submit', onSendMessage)
 
-  const leaveForm = document.querySelector('.chat.ui form')
-  leaveForm.addEventListener('submit', onLeaveRoom)
+  // Leave chat on submit
+  const leaveChat = document.querySelector('.chatleave')
+  leaveChat.addEventListener('submit', onLeaveRoom)
 
   // Socket io events
-  socket.on('join successful', loadChatUI)
-  socket.on('new_message', onMessageReceived)
-  socket.on('leave successful', leaveChatUI)
+  socket.on('join success', joinChatRoom)
+  socket.on('new_message', onReceivedMessage)
+  socket.on('disconnect', leaveChatRoom)
 }
 
 function onJoinRoom(event) {
   event.preventDefault()
-  const [nameInput, roomInput] = document.querySelectorAll('.join.ui input')
+  const [nameInput, roomInput] = document.querySelectorAll('.join input')
 
   const name = nameInput.value
   const room = roomInput.value
 
   socket.emit('join room', { name, room })
+
   const h2 = document.querySelector('h2')
   const li = document.createElement('li')
   li.classList.add('room-text')
@@ -38,37 +40,38 @@ function onJoinRoom(event) {
 }
 
 function onLeaveRoom(event) {
-   event.preventDefault()
-  
-}
+  event.preventDefault()
+  socket.emit('disconnect')
+  location.reload()
+  }
 
 function onSendMessage(event) {
   event.preventDefault()
-  const input = document.querySelector('.chat.ui form input')
+  const input = document.querySelector('.chat form input')
   socket.emit('new_message', input.value)
   input.value = ''
 }
 
-function loadChatUI(data) {
-  console.log(data)
-  document.querySelector('.join.ui').classList.add('hidden')
-  document.querySelector('.chat.ui').classList.remove('hidden')
-}
-
-function leaveChatUI(data) {
-  console.log(data)
-  document.querySelector('.chat.ui').classList.add('hidden')
-  document.querySelector('.join.ui').classList.remove('hidden')
-}
-
-function onMessageReceived({ name, message }) {
+function onReceivedMessage({ name, message }) {
   const ul = document.querySelector('ul')
   const li = document.createElement('li')
   li.innerText = `${name}: ${message}`
   ul.append(li)
 }
 
-//buttons and inputs
+function joinChatRoom(data) {
+  console.log(data)
+  document.querySelector('.join').classList.add('hidden')
+  document.querySelector('.chat').classList.remove('hidden')
+}
+
+function leaveChatRoom(data) {
+  console.log(data)
+  document.querySelector('.chat').classList.add('hidden')
+  document.querySelector('.join').classList.remove('hidden')
+}
+
+/*//buttons and inputs
 var message = $("#message")
 var name = $("#name")
 var send_message = $("#send_message")
@@ -95,4 +98,4 @@ var feedback = $("#feedback")
 	//Listen on typing
 	socket.on('typing', (data) => {
 		feedback.html("<p><i>" + data.name + " is typing a message..." + "</i></p>")
-	})
+	})*/
